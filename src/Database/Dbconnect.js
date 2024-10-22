9c6fa4d4d1b80c564c8a0d3652662bd5234b3492
Mahmudul Islam <mahmudulislammern@gmail.com>
2024-10-23 02:24:41 +0600

    little fix

--- src/Database/Dbconnect.js
+++ src/Database/Dbconnect.js
@@ -5,7 +5,8 @@ const chalk = require("chalk");
 const dbConnect = async () => {
   try {
     const dbconnectionInstense = await mongoose.connect(
-      process.env.MONGODB_DATABASE_URL || ""
+      process.env.MONGODB_DATABASE_URL
+      // process.env.MONGODB_DATABASE_URL || "" //my way
       //   there is big problem with upper code ---|| ""--- I lost two days with it.
     );
   } catch (error) {

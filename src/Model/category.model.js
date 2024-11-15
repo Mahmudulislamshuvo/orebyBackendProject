404d8d6fd24a1f2448111e13fb45044d2139d8f9
Mahmudul Islam <mahmudulislammern@gmail.com>
2024-11-15 23:39:18 +0600

    cetagory schema maded

--- src/Model/category.model.js
+++ src/Model/category.model.js
@@ -1,2 +1,19 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+
+const categoryModel = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  subCategory: {
+    type: Schema.Types.ObjectId,
+    hef: "subcategory",
+  },
+});

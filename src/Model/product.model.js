a0925631e892927b917d6af581a5d1496c3d25ef
Mahmudul Islam <mahmudulislammern@gmail.com>
2024-11-17 18:08:25 +0600

    product, store, subcategor Schema completed

--- src/Model/product.model.js
+++ src/Model/product.model.js
@@ -1,4 +1,46 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const productModel = new Schema({});
+const productModel = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    trim: true,
+  },
+  stock: {
+    type: Number,
+  },
+  rating: {
+    type: Number,
+  },
+  review: [
+    {
+      type: Number,
+      trim: true,
+    },
+  ],
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "store",
+  },
+  category: {
+    type: Schema.Types.ObjectId,
+    ref: "category",
+  },
+  subCategory: {
+    type: Schema.Types.ObjectId,
+    ref: "subcategory",
+  },
+});
+
+module.exports = mongoose.model("product", productModel);
